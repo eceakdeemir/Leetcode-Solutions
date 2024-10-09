777bebcfd6699a3e077a8591aaef121ece7795a4
eceakdeemir <121350405+eceakdeemir@users.noreply.github.com>
2024-10-09 22:14:37 +0300

    Time: 103 ms (29.24%), Space: 16.8 MB (6.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,32 +2,17 @@ from collections import Counter
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # 1. Uzunluk kontrolü
         if len(s1) > len(s2):
             return False
-
-        # 2. Karakter sayımlarını hesapla
         s1_count = Counter(s1)
         window_count = Counter(s2[:len(s1)])
-
-        # 3. İlk pencereyi kontrol et
-        if s1_count == window_count:
+        if window_count == s1_count:
             return True
-        
-        # 4. Pencereyi kaydır
-        for i in range(len(s1), len(s2)):
-            # Yeni karakter ekle
+        for i in range (len(s1), len(s2)):
             window_count[s2[i]] += 1
-            # Eski karakteri çıkar
             window_count[s2[i - len(s1)]] -= 1
-            
-            # Eğer eski karakterin sayısı sıfıra düştüyse onu kaldır
             if window_count[s2[i - len(s1)]] == 0:
                 del window_count[s2[i - len(s1)]]
-                
-            # 5. Güncellenen pencereyi kontrol et
-            if window_count == s1_count:
+            if s1_count == window_count:
                 return True
-        
-        # 6. Eşleşme bulunamazsa
         return False
