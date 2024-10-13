9b26ad9dd3d02d651e573bc243911ead3eb33558
eceakdeemir <121350405+eceakdeemir@users.noreply.github.com>
2024-10-13 22:15:17 +0300

    Time: 42 ms (63.23%), Space: 16.6 MB (42.63%) - LeetHub

--- 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
@@ -8,8 +8,17 @@ class Solution:
 
             while count[s[end]] > 2:
                 count[s[start]] -= 1
-                if count[s[start]] == 0:
-                    del count[s[start]]
+                #if count[s[start]] == 0:
+                    #del count[s[start]]
                 start += 1
             max_len = max(max_len, end - start + 1)
         return max_len
+
+        kaaabcdefg
+
+        """Python'daki .get() metodu, bir sözlükte (dictionary) belirli bir anahtarın değerini almanızı sağlar. Eğer anahtar sözlükte mevcut değilse, bu durumda bir hata vermek yerine varsayılan bir değer döndürür. Bu, hata ayıklama işlemini kolaylaştırır.
+
+Kullanımı:
+value = my_dict.get(key, default_value)
+key: Aramak istediğiniz anahtar.
+default_value: Anahtar mevcut değilse döndürülecek değer (bu parametre isteğe bağlıdır; belirtilmezse None döner)."""
