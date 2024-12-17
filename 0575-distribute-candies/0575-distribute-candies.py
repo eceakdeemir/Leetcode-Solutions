7f75d1b48601c294225c1854c7d8ae2365ba8d58
Ece Akdemir <121350405+eceakdeemir@users.noreply.github.com>
2024-12-17 22:17:04 +0300

    Time: 10 ms (96.58%), Space: 19.8 MB (8.6%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,9 +1,10 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
+        # Farklı şeker türlerini set içinde saklayarak sayısını buluyoruz
         candies = set(candyType)
-        if len(candies) > len(candyType) / 2:
-            return int(len(candyType) / 2)
-        else:
-            return int(len(candies))
+        
+        # Alice'in yiyebileceği şeker türlerinin sayısı, ya farklı türlerin sayısı ya da n/2 ile sınırlıdır
+        return min(len(candies), len(candyType) // 2)
+
 
             
