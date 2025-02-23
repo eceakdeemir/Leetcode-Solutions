480088831c65e14a082b4172b3d98f97ed1468d7
Ece Akdemir <121350405+eceakdeemir@users.noreply.github.com>
2025-02-23 20:51:12 +0300

    Time: 55 ms (17.94%), Space: 19.8 MB (58.34%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,9 +6,20 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Fast ve slow'u baştan (head) aynı noktaya yerleştiriyoruz.
         fast = slow = head
+        
+        # fast ve slow'u, listenin sonuna kadar hareket ettiriyoruz.
+        # fast her adımda iki ileriye gider, slow ise bir adımda bir ileriye gider.
         while fast and fast.next:
+            # slow bir adım ilerler, fast ise iki adım ilerler.
             slow, fast = slow.next, fast.next.next
-            if (fast == slow):
+            
+            # Eğer fast slow'a ulaşırsa, bu bir döngü olduğunu gösterir.
+            if fast == slow:
                 return True
+        
+        # Eğer fast listenin sonuna ulaşırsa ve slow ile aynı noktaya gelmezse,
+        # bu listenin döngüsüz olduğunu gösterir.
         return False
+
