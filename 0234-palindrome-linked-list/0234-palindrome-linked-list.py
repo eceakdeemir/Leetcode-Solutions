18949e61de1efffb37438625b157a53fe1506103
Ece Akdemir <121350405+eceakdeemir@users.noreply.github.com>
2025-02-23 20:53:48 +0300

    Time: 16 ms (95.27%), Space: 39.2 MB (47.83%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,11 +5,17 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # Bağlı listeyi bir diziye dönüştür
         new_list = []
         new_head = head
+        # Listeyi baştan sona kadar gez ve değerleri new_list'e ekle
         while (new_head):
-            new_list.append(new_head.val)
-            new_head = new_head.next
+            new_list.append(new_head.val)  # Düğümün değerini ekle
+            new_head = new_head.next  # Sonraki düğüme geç
+        
+        # Eğer liste, tersine eşitse palindromdur
         if (new_list == new_list[::-1]):
             return True
+        
+        # Eğer palindrom değilse, False döndür
         return False
