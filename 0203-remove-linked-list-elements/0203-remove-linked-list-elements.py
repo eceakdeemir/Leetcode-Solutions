133a4b41aad58693e7c85164faa1576f1602c986
Ece Akdemir <121350405+eceakdeemir@users.noreply.github.com>
2025-02-23 20:52:07 +0300

    Time: 2 ms (43.64%), Space: 19.7 MB (39.85%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,13 +5,20 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # Listenin başındaki düğümleri kontrol et ve 'val' ile eşleşenleri sil
         while head and head.val == val:
-            head = head.next
-        
+            head = head.next  # Başta 'val' değerine sahip düğümleri geç
+
+        # Yeni bir baş başlatıyoruz
         new_head = head
+        
+        # Listenin geri kalanını kontrol et
         while new_head and new_head.next:
+            # Eğer bir sonraki düğümde 'val' değeri varsa, o düğümü sil
             if new_head.next.val == val:
-                new_head.next = new_head.next.next
+                new_head.next = new_head.next.next  # 'new_head.next' düğümünü atla
             else:
-                new_head = new_head.next
+                new_head = new_head.next  # 'new_head' bir adım ileri git
+                
+        # Güncellenmiş başı döndür
         return head
